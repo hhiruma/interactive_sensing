02a39c07987fb0dadd7bf932c12e7e25ac49d3eb
masudam <masa.easygoing@gmail.com>
2018-06-15 18:13:00 +0900

    多次元配列にしました

--- robot/robot.ino
+++ robot/robot.ino
@@ -1,13 +1,16 @@
 #include <Chrono.h>
 #include <LightChrono.h>
+#include <Servo.h>
 
 Chrono myChrono;
 Servo myServo;
 Servo myServo2;
 
 //楽譜，０１２３で左から順,4は叩かない
-int rightSheet[] = {4,4,4,4};
-int leftSheet[] = {4,4,4,4};
+
+
+
+int sheet[2][4] ={{4,4,4,4},{4,4,4,4}};
 int nowtime = 0;
 int nowPlace[] = {0,0}; //[0]がR,[1]がL
 
@@ -18,18 +21,27 @@ void setup() {
   
 }
 
+  
+void hit(){
+  //ここで両手を叩かせる，今考えているのは角度を分割してfor文で交互に回すことによって同時のように見えさせる
+Serial.println("Hit!");
+  
+}
+
+
+
 void loop() {
   // put your main code here, to run repeatedly:
   if(myChrono.hasPassed(500)){
     //ここで楽譜を読んでアーム移動
     
-    if(rightSheet[(nowtime+1)%4] =! 4){
-      moveArm(rightSheet[(nowtime+1)%4]-nowPlace[0]);
-      nowPlace[0] = rightSheet[(nowtime+1)%4]
+    if(sheet[0][(nowtime+1)%4] =! 4){
+      moveArm(sheet[0][(nowtime+1)%4]-nowPlace[0]);
+      nowPlace[0] = sheet[0][(nowtime+1)%4];
     }
-    if(leftSheet[(nowtime+1)%4] =! 4){
-      moveArm(leftSheet[(nowtime+1)%4]-nowPlace[1]);
-      nowPlace[1] = leftSheet[(nowtime+1)%4]
+    if(sheet[1][(nowtime+1)%4] =! 4){
+      moveArm(sheet[1][(nowtime+1)%4]-nowPlace[1]);
+      nowPlace[1] = sheet[1][(nowtime+1)%4];
     }
   }
   
@@ -60,17 +72,11 @@ void moveArm(int sa){
      break;
     case 0:
      break;
-    default:
-     throw();
-    
   }
-
-
-  
-void hit(){
-  //ここで両手を叩かせる，今考えているのは角度を分割してfor文で交互に回すことによって同時のように見えさせる
-  
 }
+
+
+
 
 
 

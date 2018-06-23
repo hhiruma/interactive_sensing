491bb33e648a54f84db9d259097099cc17d72dda
hyohyogo@yahoo.co.jp <hyohyogo@yahoo.co.jp>
2018-06-24 00:05:08 +0900

    入力インターフェース作成

--- input_interface/input_interface.ino
+++ input_interface/input_interface.ino
@@ -1,5 +1,3 @@
-using namespace std;
-
 #include <Chrono.h>
 
 #define WAIT 0
@@ -14,6 +12,9 @@ const int mode_toggle_pin = 7;
 //楽譜入力ピン
 const int note_input_pin = 8;
 
+//RECORD状態表示用LED
+const int record_LED_pin = 13;
+
 //楽譜用のタイマー
 Chrono timer;
 
@@ -21,6 +22,10 @@ void setup() {
   Serial.begin(9600);
   pinMode(mode_toggle_pin, INPUT);
   pinMode(note_input_pin, INPUT);
+  pinMode(record_LED_pin, OUTPUT);
+
+  //初期モードはWAITなのでLOWに
+  digitalWrite(record_LED_pin, LOW);
 
   //初期のモードは待機
   mode = WAIT;
@@ -29,41 +34,54 @@ void setup() {
 void loop() {
   if (mode == WAIT) {
     //待機状態の場合
+    //LED表示をOFFに
+    digitalWrite(record_LED_pin, LOW);
+    //トグル用のボタンのみ読み込む
     int in_toggle = digitalRead(mode_toggle_pin);
 
     //toggle用のボタンが押されたらモードを切り替えて終了
     if (in_toggle) {
+      //processingに文字を送っても比較で失敗するのでSTARTの意味として-1を送る
+      Serial.println("-1");
+      //モードをRECORDに切り替える
       mode = RECORD;
-      Serial.println("RECORD START");
-      delay(500);
+      delay(200);
+      //このタイミングでタイマーをリセットする
       timer.restart();
       return;
     }
   } else {
     //録音状態の場合
+    //LED表示をONに
+    digitalWrite(record_LED_pin, HIGH);
+    //トグル用のボタンと音用のボタンを読み込む
     int in_toggle = digitalRead(mode_toggle_pin);
     int in_note = digitalRead(note_input_pin);
 
     if (in_toggle) {
+      //processingに文字を送っても比較で失敗するのでENDの意味として-2を送る
+      Serial.println("-2");
+      //モードをWAITに切り替える
       mode = WAIT;
-      Serial.println("RECORD END");
-      delay(500);
+      delay(200);
       return;
     }
     if (in_note) {
+      //RECORDモードで音用ボタンが押された場合
       //仮で入力する値を0番目の値とする
-      int tmp_input_note = 0;
-      
       /*
-       * 実装予定：
-       *   楽譜をprocessingに送って配列に入れる
+       * ボタンによって入力する値を変えるように実装する予定
        */
-       
-      Serial.print("note = ");
-      Serial.print(tmp_input_note);
-      Serial.print(", time =");
-      Serial.println(timer.elapsed());
-      delay(200);
+      int tmp_input_note = 1;
+
+      //入力値を送る
+      Serial.println(tmp_input_note);
+      delay(100);
+
+      //入力された時刻を送る
+      float time_value = float(timer.elapsed()) / 1000;
+      Serial.println(time_value, 5);
+      delay(100);
     }
   }
 }
---
+++ score_sender/score_sender.pde
@@ -0,0 +1,89 @@
+import processing.serial.*;
+import java.util.Queue;
+import java.util.ArrayDeque;
+
+Serial port;
+
+//check data type from serial
+//  0: note
+//  1: time
+int dataTypeToggle;
+
+float tmp_data_store1;
+float tmp_data_store2;
+
+Queue<float[]> score = new ArrayDeque<float[]>();
+
+boolean recording;
+
+final int RECORD_START = -1;
+final int RECORD_END = -2;
+
+void setup(){
+  size(100,100);
+  
+  //select serial port for arduino
+  String arduinoPort = Serial.list()[3];
+  
+  port = new Serial(this, arduinoPort, 9600);
+  
+  //initialize as note type
+  dataTypeToggle = 0;
+  
+  recording = false;
+}
+
+void draw(){
+}
+
+void serialEvent(Serial p){
+  //get value as String and then convert that to float
+  //get message till line break (ASCII > 13)
+  String message = port.readStringUntil(13);
+  if(message != null){
+    float value = float(message);
+    
+    if (!recording){
+      if(value == RECORD_START){
+        recording = true;
+        println("RECORD START");
+        return;
+      }
+    } else {
+      if(value == RECORD_END){
+        recording = false;
+        println("RECORD END");
+        outputScore();
+        return;
+      }
+      if(dataTypeToggle == 0){
+          tmp_data_store1 = value;
+          dataTypeToggle = 1;
+      } else {
+          tmp_data_store2 = value;
+          
+          println("new score: note = " + tmp_data_store1 + ", time = " + tmp_data_store2);
+          float[] tmp_data_array = {tmp_data_store1, tmp_data_store2};
+          score.add(tmp_data_array);
+          
+          dataTypeToggle = 0;
+      }
+    }
+  }
+}
+
+void outputScore(){
+  //output score as csv file
+  PrintWriter output = createWriter("score.csv");
+  float[] tmp;
+
+  while(score.size() > 0){
+    tmp = score.remove();
+    println("note=" + tmp[0] + ", time=" + tmp[1]);
+    
+    output.println(tmp[0] + "," + tmp[1]);
+  }
+  
+  output.flush();
+  output.close();
+}

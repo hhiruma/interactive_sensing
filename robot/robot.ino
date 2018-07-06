14c643e2861e9712dbef2d4ecf372a1e17a75d7b
hyohyogo@yahoo.co.jp <hyohyogo@yahoo.co.jp>
2018-07-06 19:03:32 +0900

    complieNotes作成(デバッグなし)

--- robot/robot.ino
+++ robot/robot.ino
@@ -1,7 +1,6 @@
 #include <Chrono.h>
 #include <LightChrono.h>
 #include <Servo.h>
-#include <TimedAction.h> //読み込めるかはまだチェックしてない
 
 #define RIGHT 0
 #define LEFT 1
@@ -17,6 +16,10 @@
 #define NOTE_LEN 100
 #define DRUM_NUM 6
 
+#define HIT 0
+#define MOVE 1
+#define ACTION_TIME 200
+
 //時間計測器
 Chrono timer;
 
@@ -29,24 +32,18 @@ const int shoulderAngle[2][DRUM_NUM/2] = {{134, 90, 55}, {134, 90, 55}};
 //楽譜関連
 //  読み込み先
 String str_notes[NOTE_LEN];
-//  int型の楽譜読込先 (直接使用することは無いが、デバッグ用に作っておく)
+//  int型の楽譜読込先(ここに音が演奏されるタイミングをすべて書いていく)
 int int_notes[NOTE_LEN][2];
-//  TimedAction型の楽譜読込先
-TimedAction* playThreads;
-//  TimedAction型の音再生タイミングアラーム読込先
-TimedAction* playTimeSetter;
+//  編集した楽譜
+int compiled_note[NOTE_LEN*2][3];
 //  読み込み中判定
 boolean reading_notes;
 //  完成した楽譜を持ってるか判定
 boolean has_complete_sheet;
 //  楽譜の番号
 int note_counter;
-//  TimedActionにのコールバックに渡す時に使うためのグローバル変数
-int global_selected_note;
 //  終了時刻
 int global_sheet_time;
-//  初回ループ判定
-boolean is_setup_loop;
 
 //次に打つ位置
 int rightNextPos;
@@ -65,7 +62,6 @@ void setup() {
   //楽譜読み込み状態をfalseに設定
   reading_notes = false;
   has_complete_sheet = false;
-  is_setup_loop = true;
 }
 
 void loop() {
@@ -77,36 +73,50 @@ void loop() {
     return;
   }
 
-  if(!has_complete_sheet) {
-    //最初に完璧な楽譜を持っていない状態では何もしない
-    return;
-  }
-  
-//  //セットアップ用の初回周期では
-//  if(is_setup_loop){
-//    //int_notesの各音に対して該当する値があるか確認
-////    Serial.println(String(timer.elapsed()));
-//    for(int i=0; i<note_counter; i++){
-//      if(timer.hasPassed(int_notes[i][1]) && !timer.hasPassed(int(int_notes[i][1]) + 200)){
-//        //タイミングが合ったら、スレッドを有効化する
-//        playThreads[i].reset();
-//        playThreads[i].enable();
-//        String tmp = "enable : " + i;
-//        Serial.println("enable");
-//
-//        if(i == note_counter-1){
-//          //最後の音のセットアップが完了したら
-//          is_setup_loop = false;
-//          Serial.println("setup done");
-//        }
-//      }
-//    }
-//    return;
-//  } else {
-    for(int i=0; i<note_counter; i++){
-      playThreads[i].check();
-    }
-//  }
+  // //T_MOVE ~ 5sec： reset直後、腕を動かす時間
+  // if (timer.hasPassed(T_MOVE) && timer.hasPassed(T_MOVE+5)) {
+  //   int nextCount = hitCounter + 1;
+  //   //ここで楽譜を読んでアーム移動
+
+  //   //次の位置
+  //   rightNextPos = sheet[RIGHT][nextCount % SHEET_LEN];
+  //   leftNextPos = sheet[LEFT][nextCount % SHEET_LEN];
+
+  //   //次の位置に移動
+  //   moveArm(RIGHT, rightNextPos);
+  //   //角度が未実装なため左のmoveArmはコメントアウト
+  //   moveArm(LEFT , leftNextPos);
+  // }
+
+  // //T_IHT ~ 5sec: 腕を動かした後、打つタイミング
+  // if (timer.hasPassed(T_HIT) && !timer.hasPassed(T_HIT+5)) {
+  //   if (sheet[RIGHT][hitCounter % SHEET_LEN] != -1) {
+  //     if (sheet[LEFT][hitCounter % SHEET_LEN] != -1) {
+  //       hit(BOTH);
+  //     } else {
+  //       hit(RIGHT);
+  //     }
+  //   } else {
+  //     if (sheet[LEFT][hitCounter % SHEET_LEN] != -1) {
+  //       hit(LEFT);
+  //     }
+  //   }
+  // }
+
+
+  // //T_RESET: 全ての作業が終わってると見込まれる時間
+  // if (timer.hasPassed(T_RESET)) {
+  //   //hitCounterが大きくなりすぎたとき用
+  //   if (hitCounter < 5001) {
+  //     hitCounter++;
+  //   } else {
+  //     hitCounter = (hitCounter + 1) % SHEET_LEN;
+  //   }
+
+  //   //タイマーをリセットする
+  //   timer.restart();
+  // }
+
 }
 
 void readSerial(){
@@ -114,11 +124,10 @@ void readSerial(){
   String input = Serial.readStringUntil(';');
 
   if(!reading_notes){
-    //記録中で無い場合
+    //記録中で無かった場合
     if(input.equals("start")){
       //楽譜の最初の行を受け取ったら
       reading_notes = true;
-      is_setup_loop = true;
       has_complete_sheet = false;
 
       //楽譜の番号も0に初期化
@@ -137,8 +146,9 @@ void readSerial(){
 
         //読み込んだString型のArrayを元にint型のArrayを生成する
         makeIntNotes(note_counter);
-        makeTimedActionNotes(note_counter);
-        setPlayTimer(note_counter);
+        compileNotes(note_counter);
+        return;
+//        setPlayTimer(note_counter);
       } else {
         //形式通りの楽譜ならば
         str_notes[note_counter] = input;
@@ -148,23 +158,7 @@ void readSerial(){
   }
 }
 
-int global_next_note_num = 0;
-void setPlayTimer(int len){
-  Serial.println("setPlayTimer");
-//  playTimeSetter = malloc(len);
-  for(int i=0; i<len; i++){
-    global_next_note_num = i;
-    playTimeSetter[i] = TimedAction(int_notes[i][1], enablePlayer);
-  }
-}
-
-void enablePlayer(){
-  Serial.println(String(global_next_note_num));
-  playThreads[global_next_note_num].enable();
-}
-
 void makeIntNotes(int len){
-  Serial.println("makeIntNotes");
   for(int i=0; i<len; i++){
     String tmp = str_notes[i];
 
@@ -178,41 +172,118 @@ void makeIntNotes(int len){
   }
 }
 
-void makeTimedActionNotes(int len){
-  Serial.println("makeTimedActionNotes");
-//  playThreads = malloc(len);
-  //バグ発生の可能性: makeIntNotesが全部実行し終わるまで待つのか...?
+void compileNotes(int len){
+  int prevNote = -1;
+  int counter = 0;
+
   for(int i=0; i<len; i++){
-    global_selected_note = int_notes[i][0];
-    playThreads[i] = TimedAction(global_sheet_time, playNote);
-    //作った段階ではまだ無効化しておく
-    playThreads[i].disable();
+    //使用する腕を求める
+    int currentArm = getArm(int_notes[i]);
+    int prevArm = getArm(prevNote);
+
+    //１つ前の楽譜で呼び出された音で場合分けする
+    //    ∵それによってdelayのかけ方が変わってくるから
+    //    memo: １回の行動にかかる時間はACTION_TIMEmsとする
+    if (prevArm == -1){
+      //１個前が-1　-> 今回が最初に演奏される音
+      if(counter != 0) Serial.println("error");
+
+      //とりあえず腕の位置を動かす
+      int tmp = int_notes[i][1] - ACTION_TIME;
+      if(int_notes[i][1] < ACTION_TIME){
+        tmp = 0;
+      }
+      compiled_note[counter][0] =  tmp;
+      compiled_note[counter][1] =  MOVE;
+      compiled_note[counter][2] = int_notes[i][0];
+      counter++;
+
+      //そのまま打つ
+      compiled_note[counter][0] =  int_notes[i][1];
+      compiled_note[counter][1] =  HIT;
+      compiled_note[counter][2] = -1;
+      counter++;
+    } else {
+      //２回め以降
+
+      int prevSameArmIndex;
+      if(prevArm != currentArm){
+        //前回別の腕を動かしていた場合
+
+        //今回使用する腕を前回使用したindexを得る
+        for(int j=i-1;;j--){
+          if(j < 0){
+            Serial.println("error");
+          }
+          if (getArm(int_notes[j]) == getArm(int_notes[i])){
+            prevSameArmIndex = j;
+            break;
+          }
+        }
+      } else {
+        prevSameArmIndex = i - 1;
+      }
+
+      //今回と前回の位置が一緒かを調べる
+      if(int_notes[prevSameArmIndex][0] == int_notes[i][0]){
+        //一緒の場合
+
+        //前回打ったタイミングと今回打つタイミングの差がACTION_TIMEms以下の場合ずらすかどうかを判定して実行する
+        shiftTimeDiffIfOverlapp(i, prevSameArmIndex, len, ACTION_TIME);
+
+        compiled_note[counter][0] =  int_notes[i][1];
+        compiled_note[counter][1] =  HIT;
+        compiled_note[counter][2] = -1;
+        counter++;
+      } else {
+        //違う場合
+
+        //前回打ったタイミングと今回打つタイミングの差がACTION_TIMEms以下の場合ずらすかどうかを判定して実行する
+        shiftTimeDiffIfOverlapp(i, prevSameArmIndex, len, ACTION_TIME*2);
+
+        compiled_note[counter][0] =  int_notes[i][1]-200;
+        compiled_note[counter][1] =  MOVE;
+        compiled_note[counter][2] = int_notes[i][0];
+        counter++;
+
+        compiled_note[counter][0] =  int_notes[i][1];
+        compiled_note[counter][1] =  HIT;
+        compiled_note[counter][2] = -1;
+        counter++;
+      }
+
+      prevArm = currentArm;
+    }
   }
+
+  Serial.println("done");
 }
 
-void playNote(){
-  /***/
-  Serial.println("playNote executed");
-  /***/
-  
-  //TimeActionはコールバックに引数を渡せないためグローバルに一旦入れておいたので、それを持ってくる
-  int note = global_selected_note;
-  //エラー検知用に-1に戻しておく
-  global_selected_note = -1;
+void shiftTimeDiffIfOverlapp(int index, int prevIndex, int arrLen, int a_time){
+  //前回打ったタイミングと今回打つタイミングの差がACTION_TIMEms以下の場合
+  int timeDiff = int_notes[index][1] - int_notes[prevIndex][1];
+  if(timeDiff < a_time){
+    //指定index以降のdiff[ms]だけすべての楽譜をずらす
+    for(int i=index; i<arrLen; i++){
+      int_notes[i][1] += timeDiff;
+    }
+  }
 
-  //腕を選択する
-  int chosenArm = getArm(note);
-  //腕を動かす
-  moveArm(chosenArm, note);
-  //打つ
-  hit(chosenArm);
+  /*
+  * memo: a_timeを指定しているのは場合によってずらす時間が変わるから
+  *　　　-> 動かさない場合は200ms, 動かす場合は400ms
+  */
 }
 
 int getArm(int note){
   //演奏する音の番号から腕を返す
-
-  //一時的にRIGHTを返すようにしておく
-  return RIGHT;
+  if(note >= 0 && note <=2){
+    return RIGHT;
+  } else if (note >= 3 && note <= 5){
+    return LEFT;
+  } else {
+    return -1;
+  }
 }
 
 void moveArm(int arm, int nextPos) {
